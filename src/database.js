622a110eca1940f64e211cb49ccbbfcc46105f75
dvillegas <dvillegas@dvillegas-HP-ENVY-15-Notebook-PC>
2020-03-14 17:19:45 -0300

    testeando

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
 
-    host:'8ef208b3e6cc.sn.mynetname.net:12222',
+    host:'8ef208b3e6cc.sn.mynetname.net',
     user:'root',
     password:'6200591',
     database:'cet_30'
